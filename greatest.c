0442e22ce05400fc815975ef19b9b54dbb4a9c0c
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-09 16:51:11 +0530

    Rename lar.c to greatest.c

---
+++ greatest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the number");
+scanf("%d%d%d",&a&b&c);
+if(a>b&&a>c)
+printf("a is largest");
+elseif(b>c)
+printf("b is largest");
+else
+printf("c is greatest");
+}
