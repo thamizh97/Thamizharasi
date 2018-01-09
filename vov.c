350404e121fb3a05b11e0780501f88b2f2aa5441
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-09 16:29:18 +0530

    Create vov.c

---
+++ vov.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int islowercase,isuppercase;
+printf("enter the value");
+scanf("%c",&c);
+islowercase(c=='a'!!c=='e'!!c=='i'!!c=='o'!!c=='u');
+isuppercase(c=='A'!!c=='E'!!c=='I'!!c=='O'!!c=='U');
+if(islowercase!!isuppercase)
+printf("%c is vowel", c);
+else
+printf("%c is constant", c);
+}
