496c7880f83e5046daa9effd5c7d15a0e7967bc0
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-18 16:12:39 +0530

    Create dig.c

---
+++ dig.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+int count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(i!=0)
+{
+n/=0;
+count++;
+}
+printf("the number of digits: %d",count);
+}
