76e8cde1124b25a52de2ec0b0dd0458162303be1
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-19 16:44:13 +0530

    Create oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the odd number till 1 to 7: ");
+scanf("%d", &n);
+printf("enter the all odd number till 1 to 7: ");
+scanf("%d", &n)"
+for(i=1;i<=7;i++)
+{
+if(n%2=0)
+printf("enter the number: ");
+scanf("%d", &n);
+}
+}
