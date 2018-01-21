6a0c7f1bff9a874ec2b3b70e52c24f7475d10f7d
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-21 09:53:12 +0530

    Create evennum.c

---
+++ evennum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number till 1 to 7: ");
+scanf("%d", &n);
+printf("enter all even number till 1 to 7: ");
+scanf("%d", &n);
+for(i=1;i<=7;i++)
+{
+if(n%2=1)
+printf("enter the all odd number till 1 to 7: ");
+scanf("%d", &n);
+}
+return 0;
+}
