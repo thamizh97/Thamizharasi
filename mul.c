de0fa0d48a955c7adf131b741545c48feaa4ffcc
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-21 12:10:49 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the limit");
+scanf("%d", &n);
+printf("the number divisible by %d is: ", n);
+for(i=1;i<=n;i++);
+if(i%d==0)
+printf(%d\t:, n);
+}
