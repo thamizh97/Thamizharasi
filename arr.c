dcc9f653b0379a515401169bd55ab06b2ca5ac2c
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-01-25 16:27:27 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+float arr[100];
+printf("enter total no of element(1 to 100): ");
+scanf("%d" ,&n);
+printf("\n");
+for(i=0;i<n;i++)
+{
+printf("enter the %d: ", i+1);
+scanf("%f" ,&arr[100]);
+}
+for(i=1;i++;++i)
+{
+if( arr[0] < arr[i])
+arr[0] = arr[i])
+}
+printf("the largest number=%2.f", arr[0]);
+return 0;
+}
