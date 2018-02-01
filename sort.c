091cb93948dd5bb4ced87a71624d0bdd3fd265d7
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-01 10:34:13 +0000

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,a[i],a[j];
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(j=0;j<n;j++)
+{
+if(a[i]<a[j]
+{
+t1=a[i];
+a[i]=a[j];
+a[i]=t1;
+}
+printf("the sorted list of elements");
+scanf("%d",%n);
+}
