f13b17ad28c91fbef0d9da0296cd6cfd8c2e5981
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-01 10:54:53 +0000

    Create median.c

---
+++ median.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,a[i],a[j],t;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(j=0;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j]'
+a[i]=t;
+}
+printf("the median element %d is",(n/2));
+}
+}
