cf705a4f45162b77fc7a6146f411a37d606f6485
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-01 11:18:08 +0000

    Create quick.c

---
+++ quick.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,a[i],a[j],t;
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(j=0;j<n;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[i]=t;
+}
+printf("the elements %d are",n);
+}
+}
