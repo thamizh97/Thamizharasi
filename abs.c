337a75f28e01ab0239a9dbe6c7aafdf8efe4f48e
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-01 11:31:02 +0000

    Create abs.c

---
+++ abs.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+float a.b;
+int c;
+printf("enter the values");
+scanf("%f",&a);
+printf("enter the values")
+scanf("%f",&b);
+c=a-b;
+printf("enter the values");
+scanf("%d",&c);
+}
