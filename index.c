b8472d6ba11e3541366fb5c4bed291a2ad73cc81
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-02 08:51:29 +0000

    Create index.c

---
+++ index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,a[i],n;
+printf("enter the values");
+scanf("%d",&n);
+printf("%d \n",&i);
+printf("the elements %d are: ");
+for(i=0;i<n;i++)
+{
+printf("%d%d",i,a[i]);
+scanf("%d%d",i,a[i]);
+}
+}
