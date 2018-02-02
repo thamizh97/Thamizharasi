bc2fdbd49705773cab35c8db7208a1ff9bf7b38d
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-02-02 09:27:05 +0000

    Create hm.c

---
+++ hm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int hours, minutes, totalminutes;
+  printf("enter the total minutes");
+  scanf("%d",&totalminutes);
+  {
+    totalminutes=(hours*60)+minutes;
+  }
+  printf(" the hours  %d are",hours,minutes);
+}
