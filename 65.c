b51bbf2dae83f3e1cf5d189b18f4d2dc9bf65700
thamizh97 <31654304+thamizh97@users.noreply.github.com>
2018-03-16 15:53:46 +0530

    Create 65.c

---
+++ 65.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdio.h>
+int main(void) {
+  char a[100];
+  printf("enter the string");
+  scanf("%[^\n\t]s",a);
+  if(printf("%s",a))
+  {
+    
+  }
+  return 0;  
+}
